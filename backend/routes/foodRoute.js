91c79e0653b6c2f37e6a2a1f678132db7e710b27
ShreyanshDwivedi-111 <shreyanshdwivedi.2004@gmail.com>
2024-10-05 22:32:12 +0530

    Comment add food item and remove food item from foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,27 +1,31 @@
 import express from "express";
-import multer from "multer";
+// import multer from "multer";  // --> Uncomment(Add Food Items)
 import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 
 const foodRouter = express.Router();
 
-// Image Storage Engine:
-const storage = multer.diskStorage({
-    destination: "uploads",
-    filename: (req, file, callback)=>{
-        return callback(null, `${Date.now()}${file.originalname}`)
-    }
-})
+// *********** Uncomment below code if you want to (ADD FOOD ITEMS) ***********
 
-const upload = multer({storage:storage});
+// // Image Storage Engine:
+// const storage = multer.diskStorage({
+//     destination: "uploads",
+//     filename: (req, file, callback)=>{
+//         return callback(null, `${Date.now()}${file.originalname}`)
+//     }
+// })
 
-// Create Food Item:
-foodRouter.post("/add",upload.single("image"),addFood);
+// const upload = multer({storage:storage});
+
+// // Create Food Item:
+// foodRouter.post("/add",upload.single("image"),addFood);
+
+// *********** Uncomment above code if you want to (ADD FOOD ITEMS) ***********
 
 // Read Food Item:
 foodRouter.get("/list",listFood);
 
 // Delete Food Item:
-foodRouter.post("/remove",removeFood);
+// foodRouter.post("/remove",removeFood); // --> Uncomment(Remove Food Items)
 
 
 export default foodRouter;
